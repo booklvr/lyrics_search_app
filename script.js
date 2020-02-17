1d82c8c0bd722df5bb3605575dd53876648a5079
nick <nick.a.dewaal@gmail.com>
2020-02-17 15:33:06 +0900

    first commit

---
+++ script.js
@@ -0,0 +1,106 @@
+const form = document.getElementById('form');
+const search = document.getElementById('search');
+const result = document.getElementById('result');
+const more = document.getElementById('more');
+
+
+const apiURL = 'https://api.lyrics.ovh';
+
+// Search by song or artist
+async function searchSong(term) {
+  // OLD PROMISE METHOD
+  // fetch(`${apiURL}/suggest/${term}`)
+  //   .then(res => res.json())
+  //   .then(data => console.log(data));
+
+  //ASYNC AWAIT PROMISE METHOD
+  const res = await fetch(`${apiURL}/suggest/${term}`);
+  const data = await res.json();
+
+  showData(data);
+}
+
+// Show song and artist in DOM
+function showData(data) {
+  // let output = '';
+
+  // data.data.forEach(song => {
+  //   output += `
+  //     <li>
+  //       <span><strong>${song.artist.name}</strong> - ${song.title}</span>
+  //       <button class="btn" data-artist="${song.artist.name}" data-songtitle="${song.title}">Get Lyrics</button>
+  //     </li>
+  //   `
+  // })
+
+  // result.innerHTML = `
+  //   <ul class="songs">
+  //     ${output}
+  //   </ul>
+  // `
+
+  result.innerHTML = `
+    <ul class="songs">
+      ${data.data.map(song => `<li>
+          <span><strong>${song.artist.name}</strong> - ${song.title}</span>
+          <button class="btn" data-artist="${song.artist.name}" data-songtitle="${song.title}">Get Lyrics</button>
+        </li>`)
+        .join('')
+    }
+    </ul>
+  `;
+
+
+  if(data.prev || data.next) {
+    more.innerHTML = `
+      ${data.prev ? `<button class="btn" onclick="getMoreSongs('${data.prev}')">Prev</button>` : ''}
+      ${data.next ? `<button class="btn" onclick="getMoreSongs('${data.next}')">Next</button>` : ''}
+    `;
+  } else {
+    more.innerHTML = '';
+  }
+}
+
+// Get prev and next songs
+async function getMoreSongs(url) {
+  const res = await fetch(`https://cors-anywhere.herokuapp.com/${url}`);
+  const data = await res.json();
+
+  showData(data);
+}
+
+// Get lyrics for song
+async function getLyrics(artist, songTitle) {
+  const res = await fetch(`${apiURL}/v1/${artist}/${songTitle}`);
+  const data = await res.json();
+
+  console.log(data);
+}
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+
+  const searchTerm = search.value.trim();
+
+  if(!searchTerm) {
+    return alert('Please at least search for something!');
+  }
+
+  searchSong(searchTerm);
+})
+
+// Get lyrics button click
+result.addEventListener('click', e => {
+
+  const clickedEl = e.target;
+
+  if (clickedEl.tagName === 'BUTTON') {
+    const artist = clickedEl.getAttribute('data-artist');
+    const songTitle = clickedEl.getAttribute('data-songtitle');
+
+    getLyrics(artist, songTitle);
+  }
+
+
+
+})
